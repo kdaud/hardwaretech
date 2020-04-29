f6b2e38b29de079b27ead21608f62aed9599fa74
kdaud <dkakumirizii@gmail.com>
2020-04-29 20:38:46 +0300

    hardware codes

---
+++ src/hardware/Programmers.java
@@ -0,0 +1,13 @@
+package hardware;
+
+public class Programmers {
+	static {
+		System.out.println("I love this coding carrier! I want to be a software engineer someday");
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
