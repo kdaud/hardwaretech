cb9b34775007965cb856e2ff423bdb478b076de3
kdaud <dkakumirizii@gmail.com>
2020-04-29 21:06:37 +0300

    files

--- src/hardware/Programmers.java
+++ src/hardware/Programmers.java
@@ -1,12 +1,21 @@
 package hardware;
 
 public class Programmers {
+	     static int age;
 	static {
+		//Scanner scan = new Scanner(System.in);
+		System.out.println("Enter your age: ");
+		 //age = scan.nextInt();
+		
+	}
+		static {
 		System.out.println("I love this coding carrier! I want to be a software engineer someday");
+	
 	}
 
 	public static void main(String[] args) {
 		
+		System.out.println("Your aged "+age);
 
 	}
 
